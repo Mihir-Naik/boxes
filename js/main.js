30b63a81b900a288081fe5e056eaf80f40501f71
Mihir Naik <mdnaik17@gmail.com>
2017-08-28 10:06:22 -0700

    set up button listener

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function sayHello(name) {
+    return "Hello " + (name || "anonymous person")
+}
+
+function setH1(string) {
+    var h1 = document.querySelector('h1')
+    h1.innerText = string
+
+}
+
+function showGreeting() {
+    setH1(sayHello("Mihir"))
+}
+var button = document.querySelector('button')
+button.addEventListener("mouseout", showGreeting)
+
+button.addEventListener("click", function () {
+    setH1(sayHello("Banana"))
+})
